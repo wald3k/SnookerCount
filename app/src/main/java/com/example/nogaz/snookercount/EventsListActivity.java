3e669dcabec5e299d0cc0d75928638c3bb459190
Waldemar Sobiecki <waldek.sob@gmail.com>
2017-02-20 18:52:06 +0100

    Added registering/unregistering BroadcastReceiver. Added mechanism for checking if connection is established before downloading a list of events. Added ScrollView for 3-Players game mode.

--- app/src/main/java/com/example/nogaz/snookercount/EventsListActivity.java
+++ app/src/main/java/com/example/nogaz/snookercount/EventsListActivity.java
@@ -3,8 +3,14 @@ package com.example.nogaz.snookercount;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.DataSetObserver;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +18,12 @@ import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.example.nogaz.snookercount.clientApi.RankingAsyncTaskDownloader;
 import com.example.nogaz.snookercount.clientApi.SnookerEvent;
 import com.example.nogaz.snookercount.clientApi.SnookerEventAdapter;
+import com.example.nogaz.snookercount.connection.ConnectionReceiver;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -33,26 +41,67 @@ public class EventsListActivity extends Activity {
 
     RankingAsyncTaskDownloader rankingAsyncTaskDownloader = null;
     ListView eventsListView;
-
     ArrayList<SnookerEvent> eventList;
     SnookerEventAdapter snookerEventAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if(ConnectionReceiver.isReciverOn(this) == false){
+            ConnectionReceiver.enableReceiver(this);
+        }
         setContentView(R.layout.snooker_events_layout);
-
         eventList = new ArrayList<SnookerEvent>();
         snookerEventAdapter = new SnookerEventAdapter(this, eventList);
-
         eventsListView = (ListView)findViewById(R.id.eventListView);
         eventsListView.setAdapter(snookerEventAdapter);
-
-        rankingAsyncTaskDownloader = new RankingAsyncTaskDownloader(this);
-        rankingAsyncTaskDownloader.execute();
+        this.launchAsyncTaskDownloader();
     }
+    private void launchAsyncTaskDownloader(){
+        if(ConnectionReceiver.isNetworkAvailable(this) == true){
+            rankingAsyncTaskDownloader = new RankingAsyncTaskDownloader(this);
+            rankingAsyncTaskDownloader.execute();
+        }
+        else{
+            Toast.makeText(this, "Cannot download event list...", Toast.LENGTH_SHORT).show();
+            this.confirmDialog(this);
+        }
+    }
+    private void confirmDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        final Context myContext = context;
+        builder
+                .setMessage("No connection")
+                .setPositiveButton("Try again",  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        launchAsyncTaskDownloader();
+                    }
+                })
+                .setNegativeButton("Go back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog,int id) {
+                        finish();//finish EventListActivity
+                    }
+                })
+                .show();
+    }
+    public boolean isNetworkAvailable(){
+        final ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        final NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
+        final NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if( wifi.isAvailable() || mobile.isAvailable() ){
+            Log.d("CONNECTION_RECEIVER", "Network Connected");
+            Toast.makeText(this, "Connected to network", Toast.LENGTH_SHORT).show();
+            return true;
+        }else{
+            Toast.makeText(this, "Disconnected from network", Toast.LENGTH_SHORT).show();
+            Log.d("CONNECTION_RECEIVER", "Network Disconnected");
+            return false;
+        }
+    }
     @Override
     protected Dialog onCreateDialog(int id) {
         switch (id) {
@@ -84,5 +133,13 @@ public class EventsListActivity extends Activity {
                 e.printStackTrace();
             }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(ConnectionReceiver.isReciverOn(this)){
+            ConnectionReceiver.disableReceiver(this);
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/nogaz/snookercount/connection/ConnectionReceiver.java
+++ app/src/main/java/com/example/nogaz/snookercount/connection/ConnectionReceiver.java
@@ -1,8 +1,10 @@
 package com.example.nogaz.snookercount.connection;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
@@ -15,20 +17,73 @@ import android.widget.Toast;
 public class ConnectionReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("CONNECTION_RECEIVER", "Metoda onReceive");
-        final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        final NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
-        final NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-
-        if( wifi.isAvailable() || mobile.isAvailable() ){
-            Log.d("CONNECTION_RECEIVER", "Network Connected");
+        if(isNetworkAvailable(context)){
             Toast.makeText(context.getApplicationContext(), "Connected to network", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(context.getApplicationContext(), "Disconnected from network", Toast.LENGTH_SHORT).show();
-            Log.d("CONNECTION_RECEIVER", "Network Disconnected");
         }
+        else{
+            Toast.makeText(context.getApplicationContext(), "Disconnected from network", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    /**
+     * Checks if device is connected to WIFI or MOBILE NETWORK
+     * @param context
+     * @return boolean true if device is connected
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        int[] networkTypes = {ConnectivityManager.TYPE_MOBILE,
+                ConnectivityManager.TYPE_WIFI};
+        try {
+            ConnectivityManager connectivityManager =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            for (int networkType : networkTypes) {
+                if (activeNetworkInfo != null &&
+                        activeNetworkInfo.getType() == networkType)
+                    return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Checks if broadcastreceiver is turned on.
+     * @param context
+     * @return
+     */
+    public static boolean isReciverOn(Context context){
+        ComponentName component = new ComponentName(context, ConnectionReceiver.class);
+
+        int status = context.getPackageManager().getComponentEnabledSetting(component);
+        if(status == PackageManager.COMPONENT_ENABLED_STATE_ENABLED) {
+            Log.d("ConnectionReceiver","receiver is enabled");
+            return  true;
+        } else if(status == PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
+            Log.d("ConnectionReceiver","receiver is disabled");
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Enables broadcast receiver for WIFI and MOBILE NETWORK
+     * @param context
+     */
+    public static void enableReceiver(Context context){
+        ComponentName component = new ComponentName(context, ConnectionReceiver.class);
+        context.getPackageManager().setComponentEnabledSetting(component, PackageManager.COMPONENT_ENABLED_STATE_ENABLED , PackageManager.DONT_KILL_APP);
+        Toast.makeText(context.getApplicationContext(), "Broadcast receiver activated", Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Disables broadcast receiver for WIFI and MOBILE NETWORK
+     * @param context
+     */
+    public static void disableReceiver(Context context){
+        ComponentName component = new ComponentName(context, ConnectionReceiver.class);
+        context.getPackageManager().setComponentEnabledSetting(component, PackageManager.COMPONENT_ENABLED_STATE_DISABLED , PackageManager.DONT_KILL_APP);
+        Toast.makeText(context.getApplicationContext(), "Broadcast receiver deactivated", Toast.LENGTH_SHORT).show();
     }
 }
